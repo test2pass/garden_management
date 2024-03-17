e06c5bc70c0ec69fcf3a5ef519227c9f5b79ba60
test2pass <zmudarobert0@gmail.com>
2024-03-17 18:13:46 +0100

    members details, migration, all members templates add

--- garden_management/members/models.py
+++ garden_management/members/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#All models created here will be add to db.sqlite3 as a tables
+class Member(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    e_mail = models.CharField(max_length = 255, null = True)
+    joined_date = models.DateField(null = True)
